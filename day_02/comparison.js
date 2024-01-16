00a4a35e2e380b15bd154d7c5715ddf31813a4a1
Its_Rajdeep <130769261+ItsRajdeep@users.noreply.github.com>
2024-01-16 20:14:32 +0000

    day_02

---
+++ day_02/comparison.js
@@ -0,0 +1,20 @@
+// beyond the if else comparison
+
+console.log("2" > 1); //true
+console.log("02" > 1);//true
+
+console.log("2" === 2);//false
+
+
+
+console.log(null == 0);//false
+console.log(null > 0);//false
+console.log(null >= 0);//tue
+console.log(null < 0);//false
+console.log(null <= 0);//true
+
+
+
+console.log(undefined == 0);//false
+console.log(undefined >= 0);//false
+console.log(undefined <= 0);//false
