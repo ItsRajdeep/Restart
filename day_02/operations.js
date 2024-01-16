d4a05d75b63fbb58ab12f271b2802df037115d18
Its_Rajdeep <130769261+ItsRajdeep@users.noreply.github.com>
2024-01-16 17:07:17 +0000

    day_02

--- day_02/operations.js
+++ day_02/operations.js
@@ -1,1 +1,27 @@
-let a=2;
+//facts of operations...
+
+let str1="Hello ";
+let str2="World";
+let str3=str1 + str2;
+
+//console.log(str3); //we can add two strings but not subtract
+
+console.log("1"+2+2); //if there is a string in first then the next numerics are also converted into string
+console.log(1+2+"2"); 
+//if the string is present in last then first the numbers are calculated first then the string is added in last
+
+
+console.log(+true)//1
+
+console.log(+false)//0
+
+console.log(+"")//0
+
+let value = 5;
+++value //6
+console.log(value)//6
+
+let value2 = 5;
+value2++ //5
+console.log(value2)//6
+
