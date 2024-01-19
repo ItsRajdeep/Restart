2cdb87805b6f9fa67bfa8ca068b9d240d6d59ad7
Its_Rajdeep <130769261+ItsRajdeep@users.noreply.github.com>
2024-01-19 09:08:52 +0000

    day_04

--- day_04/objects.js
+++ day_04/objects.js
@@ -21,13 +21,13 @@ const jsUser ={
 
 
 //How to lock the object 
-Object.freeze(jsUser);
+// Object.freeze(jsUser);
 
 jsUser.name="raj"
 console.log(jsUser.name) //Rajdeep
 
 jsUser.greeting = ()=>{
-     console.log("Hello");
+     console.log("Hello"); 
 }
 
 jsUser.greeting(); 
@@ -35,3 +35,41 @@ jsUser.greeting();
 /*
     we can't add keys in objects when we use Object.freeze;   
 */
+
+
+// adding two objects
+const obj1 ={
+    1:"a",
+    2:"b"
+}
+const obj2 ={
+    3:"a",
+    4:"b"
+}
+
+const obj3 = Object.assign({}, obj1 , obj2)
+
+console.log(obj3)
+
+//{} is the target and rest of the things are source
+
+const obj4={...obj1, ...obj2}
+
+console.log(obj4)
+
+console.log(Object.keys(obj1));
+console.log(Object.values(obj1));
+
+const user =[
+    {
+        name:"Rajdeep"
+    },
+    {
+        email:"raj@gmail.com"
+    },
+    {
+        num:1234567891
+    }
+]
+const a= user[0].name
+console.log(a);
