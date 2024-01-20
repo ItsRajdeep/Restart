9b3f369823a8c2df44102baaa9f05ee2b7cdaa33
Its_Rajdeep <130769261+ItsRajdeep@users.noreply.github.com>
2024-01-20 09:21:29 +0000

    day_05

---
+++ day_05/arrow.js
@@ -0,0 +1,13 @@
+// Syntax
+
+const funName =()=>{
+    return`Hello`;
+}
+console.log(funName())
+
+
+//more simplified form
+
+const funName2 =() => "Hello from funcName2";
+
+console.log(funName2())
---
+++ day_05/iife.js
@@ -0,0 +1,9 @@
+//Immediately Invoked Function Expression
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+//IIFE is used to Immediate execution of a function 
+// and to Remove the pollution in global scope variables
