1c97a75114de8f63857968d9cd46131bcf3766ee
Its_Rajdeep <130769261+ItsRajdeep@users.noreply.github.com>
2024-01-23 18:40:26 +0000

    day_07

--- day_07/script.js
+++ day_07/script.js
@@ -3,16 +3,17 @@ const requestUrl = 'https://api.github.com/users/ItsRajdeep'
         const xhr = new XMLHttpRequest();
         xhr.open('GET', requestUrl)
         xhr.onreadystatechange = function(){
-            console.log(xhr.readyState);
+            // console.log(xhr.readyState);
             if (xhr.readyState === 4) {
                 const data = JSON.parse(this.responseText)
                 // console.log(typeof data);
-                console.log(data.followers);
-                console.log(data);
+                // console.log(data.followers);
+                // console.log(data);
                 document.getElementById("follower").innerHTML=data.followers
                 let image = data.avatar_url;
                 // console.log(image)
                 document.getElementById("imgbox").style.backgroundImage="url('https://avatars.githubusercontent.com/u/130769261?v=4')"
+                console.log("hello")
             }
         }
         
