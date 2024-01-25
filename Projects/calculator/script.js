7709cc5bf90403c6e2ca063c68c668a38d0b312d
Its_Rajdeep <130769261+ItsRajdeep@users.noreply.github.com>
2024-01-25 18:12:39 +0000

    addDeleteButton

--- Projects/calculator/script.js
+++ Projects/calculator/script.js
@@ -14,12 +14,18 @@ Array.from(buttons).forEach((button) => {
             document.querySelector('input').value=string
 
         }
-        
+        else if(e.target.innerHTML=="x"){
+            let multiply="*";
+            string=string+multiply;
+            document.querySelector('input').value=string
+        }
+        else if(e.target.innerHTML=="DEL"){
+            string = string.toString().slice(0, -1)
+            document.querySelector('input').value=string
+        }
         else{
             string= string + e.target.innerHTML;
             document.querySelector('input').value=string
-            
         }
-        // console.log(e.target.innerHTML)
     })
 });
