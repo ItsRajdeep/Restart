5c5054478168cd22ca5c63b2c1d400f23cea9813
Its_Rajdeep <130769261+ItsRajdeep@users.noreply.github.com>
2024-01-27 04:18:59 +0000

    addFunctionality

--- Projects/digitalclock/clock.js
+++ Projects/digitalclock/clock.js
@@ -1,1 +1,62 @@
-let date = new Date();
+setInterval(() => {
+    
+    let date = new Date();
+    let day =date.getDay();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let d = date.getDate();
+let months =date.getMonth()+1;
+let years = date.getFullYear();
+
+
+const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let dayName=days[day];
+
+document.getElementById("day").innerHTML=dayName;
+
+
+if(hours < 10){
+ document.getElementById("meridian").innerHTML="AM"
+ document.getElementById("hour").innerHTML="0"+ hours;
+}
+else if(hours<=12){
+    document.getElementById("meridian").innerHTML="AM"
+    document.getElementById("hour").innerHTML=hours;
+}
+else{
+    document.getElementById("meridian").innerHTML="PM"   
+    document.getElementById("hour").innerHTML=hours-12;  
+}
+
+
+document.getElementById("minute").innerHTML=minutes;
+
+
+if(seconds<10){
+    document.getElementById("second").innerHTML="0"+seconds;
+}
+else{
+    document.getElementById("second").innerHTML=seconds;
+}
+
+
+if(d<10){
+    document.getElementById("date").innerHTML="0"+d;
+}
+else{
+    document.getElementById("date").innerHTML=d;
+}
+
+
+if(months<10){
+    document.getElementById("month").innerHTML="0"+months;
+}
+else{
+    document.getElementById("month").innerHTML=months;
+}
+
+document.getElementById("year").innerHTML=years;
+
+
+}, 1000);
